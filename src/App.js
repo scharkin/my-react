429e09eb8bceaaa2dc5b608b471e3770473d7f7d
Sergey Charkin <scharkin@deloitte.com>
2016-12-28 15:50:51 -0500

    example 1-3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// Parent Component
+class GroceryList extends Component {
+  render() {
+    return (
+      <ul>
+        <ListItem quantity="1" name="Bread" />
+        <ListItem quantity="6" name="Eggs" />
+        <ListItem quantity="2" name="Milk" />
+      </ul>
+    );
+  }
+}
 
+// Child Component
+class ListItem extends Component {
+  render() {
+    return (
+      <li>
+        {this.props.quantity}× {this.props.name}
+      </li>
+    );
+  }
+}
 class App extends Component {
   render() {
-    var place = "World";
     return (
-      <h1>Hello {place}</h1>
+      <GroceryList />
     );
   }
 }
