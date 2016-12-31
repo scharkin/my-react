26d02fe33b892e330d14759b072b5018462b07ee
Sergey Charkin <sergey.charkin@gmail.com>
2016-12-30 19:23:36 -0500

    add component props

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import { render } from 'react-dom'
 import { SkiDayCount } from './components/SkiDayCount'
 
 render(
-	<SkiDayCount />,
+	<SkiDayCount total={50}
+				 powder={20}
+				 backcountry={10}
+				 goal={100}/>,
 	document.getElementById('react-container')
 )
