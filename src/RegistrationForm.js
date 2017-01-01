852ae0f12d5d38bb3d212f5b0d5627edef700af2
Sergey Charkin <sergey.charkin@gmail.com>
2017-01-01 12:35:00 -0500

    submit form

---
+++ src/RegistrationForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class RegistrationForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log("Email submitted", this.state.email);
+  }
+
+  handleEmailChange(event) {
+    console.log("Email changed", event.target.value);
+    this.setState({email: event.target.value});
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="E-mail"
+          value={this.state.email}
+          onChange={this.handleEmailChange}
+        />
+        <button>Save</button>
+      </form>
+    )
+  }
+}
+
+export default RegistrationForm;
