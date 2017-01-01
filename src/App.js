e3d6d9f01d3a3a6724e114577abad2a48636a182
Sergey Charkin <sergey.charkin@gmail.com>
2017-01-01 14:46:00 -0500

    ref

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import RegistrationForm from './RegistrationForm';
-import './App.css';
 
 class App extends Component {
+  submit() {
+    console.log('submit', this.testInput.value);
+  }
   render() {
     return (
-      <div className="container">
-        <RegistrationForm />
+      <div>
+        <input type="text" placeholder="test"
+          ref={(input) => this.testInput = input}
+        />
+        <button onClick={this.submit.bind(this)}>Submit</button>
       </div>
     )
   }
