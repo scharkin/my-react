1dad77781dae97cf023da6620a4710ba3e093454
Sergey Charkin <sergey.charkin@gmail.com>
2017-01-02 14:23:22 -0500

    search

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ class App extends Component {
     this.props.onAddTrack(this.trackInput.value);
     this.trackInput.value = ''
   }
+  findTrack(){
+    console.log("searchInput", this.searchInput.value);
+    this.props.onFindTrack(this.searchInput.value)
+  }
 
   render() {
     console.log(this.props.tracks);
     return (
       <div>
-        <input type="text" ref={ input => this.trackInput = input } />
-        <button onClick={this.addTrack.bind(this)}>Add track</button>
+        <div>
+          <input type="text" ref={ input => this.trackInput = input } />
+          <button onClick={this.addTrack.bind(this)}>Add track</button>
+        </div>
+        <div>
+          <input type="text" ref={ input => this.searchInput = input } />
+          <button onClick={this.findTrack.bind(this)}>Find track</button>
+        </div>
         <ul>
           {this.props.tracks.map((track, index) =>
-            <li key={index}>{track}</li>
+            <li key={index}>{track.name}</li>
           )}
         </ul>
       </div>
@@ -26,9 +36,15 @@ class App extends Component {
 
 export default connect(
   state => ({
-    tracks: state.tracks
+    tracks: state.tracks.filter(track => track.name.includes(state.filterTracks))
   }),
   dispatch => ({
-    onAddTrack: trackName => dispatch({type:'ADD_TRACK', payload:trackName})
+    onAddTrack: name => {
+      const payload = { id:Date.now().toString(), name };
+      dispatch({ type:'ADD_TRACK', payload })
+    },
+    onFindTrack: name => {
+      dispatch({ type:'FIND_TRACK', payload:name })
+    }
   })
 )(App);
