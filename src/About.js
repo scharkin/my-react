29a06c5ea37a91c742db9b59d0b437e50d869752
Sergey Charkin <sergey.charkin@gmail.com>
2017-01-07 17:12:41 -0500

    react-router

---
+++ src/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Menu from './Menu';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <Menu />
+        This is our cool music app
+      </div>
+    )
+  }
+}
